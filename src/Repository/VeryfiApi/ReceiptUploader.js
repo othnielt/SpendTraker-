4c2ea4eba18d5c8ed80c1929a30443b6224b525a
othnielt <62716703+othnielt@users.noreply.github.com>
2023-06-05 15:03:20 -0400

    Update ReceiptUploader.js

--- src/Repository/VeryfiApi/ReceiptUploader.js
+++ src/Repository/VeryfiApi/ReceiptUploader.js
@@ -10,11 +10,11 @@ export const ReceiptUploader = ({ currentUser, budgetID, onClose }) => {
     reader.onloadend = async () => {
       const imageBase64 = reader.result.replace(/^data:image\/(png|jpg|jpeg);base64,/, '');
 
-      // Veryfi API credentials
-      const clientId = 'vrfJ3iyFF57hGKb3YE18PtBEUJyLUE9GxyAoWfV';
-      const clientSecret = 'hpNmOuFrHejYsxf5PjMuGlYi7FjB2e1Eu9rWIb3OPG2voXEOnDaz38GR3yeaE0G7d35URvREYBAeMbHv7TWuXUrntPoDOA63RbYh2h4nX0heznxXmRke2Sro37jKkitJ';
-      const username = 'othniel99.ot';
-      const apiKey = 'b77770908d10ee50ede5743da685a46d';
+      // repalce by your own Veryfi API credentials
+      const clientId = '';
+      const clientSecret = '';
+      const username = '';
+      const apiKey = '';
 
       const response = await ProcessReceipt(imageBase64, clientId, clientSecret, username, apiKey);
 
