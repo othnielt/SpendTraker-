e54ea1e55c54ae1af6f28dfd628c3db8dc31537f
othnielt <62716703+othnielt@users.noreply.github.com>
2023-06-05 15:04:14 -0400

    Update BudgetUploader.js

--- src/Repository/VeryfiApi/BudgetUploader.js
+++ src/Repository/VeryfiApi/BudgetUploader.js
@@ -8,11 +8,11 @@ export async function BudgetUploader(currentUser, image, amount) {
         const imageBase64 = reader.result.replace(/^data:image\/(png|jpg|jpeg);base64,/, '');
         console.log(typeof imageBase64, "imageBase64");
 
-        // Verify API credentials
-        const clientId = 'vrfJ3iyFF57hGKb3YE18PtBEUJyLUE9GxyAoWfV';
-        const clientSecret = 'hpNmOuFrHejYsxf5PjMuGlYi7FjB2e1Eu9rWIb3OPG2voXEOnDaz38GR3yeaE0G7d35URvREYBAeMbHv7TWuXUrntPoDOA63RbYh2h4nX0heznxXmRke2Sro37jKkitJ';
-        const username = 'othniel99.ot';
-        const apiKey = 'b77770908d10ee50ede5743da685a46d';
+        //replace by your own  Verify API credentials
+        const clientId = '';
+        const clientSecret = '';
+        const username = '';
+        const apiKey = '';
 
         const response = await ProcessReceipt(imageBase64, clientId, clientSecret, username, apiKey);
 
