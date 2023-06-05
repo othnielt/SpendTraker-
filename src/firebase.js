cabb1fd0aad75ef458f70e045e8c0725cb673392
othnielt <62716703+othnielt@users.noreply.github.com>
2023-06-05 15:00:34 -0400

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -5,15 +5,9 @@ import { getFirestore } from "firebase/firestore";
 
 
 
-
+// reaplace by your own firebase configuration 
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyDl6KGyq_WBEsMO0mwfrTe8-Go1ILekLOM",
-  authDomain: "csi4900-dev.firebaseapp.com",
-  projectId: "csi4900-dev",
-  storageBucket: "csi4900-dev.appspot.com",
-  messagingSenderId: "1015760265111",
-  appId: "1:1015760265111:web:bc347c308b93d669d0aed2",
-  measurementId: "G-72GKTBP4ZS"
+
 });
 
 
